e3726b133d2c0b435fe59298c44397ea4b13011e
theGraduateJobs <117873087+theGraduateJobs@users.noreply.github.com>
2022-11-11 17:50:12 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8530428648221526"
+  crossorigin="anonymous"></script>
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
